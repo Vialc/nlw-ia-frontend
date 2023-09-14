c59f5880f23d36feb462b569fca23d8d428eaed0
Vialc <vitor.alcantara@sysmap.com.br>
2023-09-14 02:33:20 -0300

    terminei

--- src/App.tsx
+++ src/App.tsx
@@ -1,106 +1,116 @@
-import { FileVideo, Github, Upload, Wand2 } from "lucide-react";
-import { Button } from "./components/ui/button";
-import { Separator } from "./components/ui/separator";
-import { Textarea } from "./components/ui/textarea";
-import { Label } from "./components/ui/label";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
-import { Slider } from "./components/ui/slider";
+import { Github, Wand2 } from 'lucide-react'
+import { Button } from './components/ui/button'
+import { Separator } from './components/ui/separator'
+import { Textarea } from './components/ui/textarea'
+import { Label } from './components/ui/label'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './components/ui/select'
+import { Slider } from './components/ui/slider'
+import { VideoInputForm } from './components/video-input.form'
+import { PromptSelect } from './components/prompt-select'
+import { useState } from 'react'
+import { useCompletion } from 'ai/react'
 
 export function App() {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <div className="px-6 py-3 flex items-center justify-between border-b">
-        <h1 className="text-xl font-bold">upload.ai</h1>
+  const [temperature, setTemperature] = useState(0.5)
+  const [videoId, setVideoId] = useState<string | null>(null)
 
-        <div className="flex items-center gap-3">
-          <span className="text-sm text-muted-foreground">Desenvolvido com ❤️ no NLW da Rocketseat</span>
-          <Separator orientation="vertical" className="h-6" />
-          <Button variant="outline">
-            <Github className="w-4 h-4 mr-2" />
+  const { input, setInput, handleInputChange, handleSubmit, completion, isLoading } = useCompletion({
+    api: 'http://localhost:3333/ai/complete',
+    body: {
+      videoId,
+      temperature,
+    },
+    headers: {
+      "Content-type": 'application/json'
+    }
+  })
+  return (
+    <div className='min-h-screen flex flex-col'>
+      <div className='px-6 py-3 flex items-center justify-between border-b'>
+        <h1 className='text-xl font-bold'>upload.ai</h1>
+
+        <div className='flex items-center gap-3'>
+          <span className='text-sm text-muted-foreground'>
+            Desenvolvido com ❤️ no NLW da Rocketseat
+          </span>
+          <Separator orientation='vertical' className='h-6' />
+          <Button variant='outline'>
+            <Github className='w-4 h-4 mr-2' />
             Github
-            </Button>
+          </Button>
         </div>
       </div>
-      <main className="flex-1 p-6 flex gap-6">
-        <div className="flex flex-1 flex-col gap-4">
-          <div className="grid grid-rows-2 gap-4 flex-1">
-            <Textarea placeholder="Inclua o prompt para a IA..." className="resize-none p-4 leading-relaxed" />
-            <Textarea placeholder="Resultado gerado pela IA..." className="resize-none p-4 leading-relaxed" readOnly />
+      <main className='flex-1 p-6 flex gap-6'>
+        <div className='flex flex-1 flex-col gap-4'>
+          <div className='grid grid-rows-2 gap-4 flex-1'>
+            <Textarea
+              placeholder='Inclua o prompt para a IA...'
+              className='resize-none p-4 leading-relaxed'
+              value={input}
+              onChange={handleInputChange}
+            />
+            <Textarea
+              placeholder='Resultado gerado pela IA...'
+              className='resize-none p-4 leading-relaxed'
+              readOnly
+              value={completion}
+            />
           </div>
-          <p className="text-sm text-muted-foreground">
-            Lembre-se: você pode utilizar a variável <code className="text-violet-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do video selecionado.
-            </p>
+          <p className='text-sm text-muted-foreground'>
+            Lembre-se: você pode utilizar a variável{' '}
+            <code className='text-violet-400'>{'{transcription}'}</code> no seu
+            prompt para adicionar o conteúdo da transcrição do video
+            selecionado.
+          </p>
         </div>
-        <aside className="w-80 space-y-6">
-          <form className="space-y-6">
-            <label htmlFor="video" className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5">
-              <FileVideo className="w-4 h-4" />
-              Selecione um video
-            </label>
-            <input type="file" id="video" accept="video/mp4" className="sr-only" />
-
-            <Separator />
-            <div className="space-y-2">
-              <Label htmlFor="transcription_prompt">
-                Prompt de transcrição
-              </Label>
-              <Textarea
-              id="transcription_prompt"
-              className="h-20 leading-relaxed resize-none"
-              placeholder="Inclua palavras-chave mencionadas no video separadas por vírgula (,)"
-              />
-            </div>
-            <Button type="submit" className="w-full">
-              Carregar video
-              <Upload className="w-4 h-4 ml-2"/>
-            </Button>
-          </form>
+        <aside className='w-80 space-y-6'>
+          <VideoInputForm onVideoUploaded={setVideoId} />
           <Separator />
-          <form className="space-y-6">
-
-          <div className="space-y-2">
+          <form className='space-y-6' onSubmit={handleSubmit}>
+            <div className='space-y-2'>
               <Label>Prompt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt..." />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="value">Titulo do YouTube</SelectItem>
-                  <SelectItem value="description">Descrição do YouTube</SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect onPromptSelected={setInput} />
             </div>
 
-            <div className="space-y-2">
+            <div className='space-y-2'>
               <Label>Modelo</Label>
-              <Select disabled defaultValue="gpt3.5">
+              <Select disabled defaultValue='gpt3.5'>
                 <SelectTrigger>
                   <SelectValue />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
+                  <SelectItem value='gpt3.5'>GPT 3.5-turbo 16k</SelectItem>
                 </SelectContent>
               </Select>
-              <span className="block text-xs text-muted-foreground italic">
+              <span className='block text-xs text-muted-foreground italic'>
                 Você poderá customizar essa opção em breve
               </span>
             </div>
             <Separator />
-            <div className="space-y-4">
+            <div className='space-y-4'>
               <Label>Temperatura</Label>
               <Slider
-              min={0}
-              max={1}
-              step={0.1}
+                min={0}
+                max={1}
+                step={0.1}
+                value={[temperature]}
+                onValueChange={(value) => setTemperature(value[0])}
               />
-              <span className="block text-xs text-muted-foreground italic">
-                Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros.
+              <span className='block text-xs text-muted-foreground italic'>
+                Valores mais altos tendem a deixar o resultado mais criativo e
+                com possíveis erros.
               </span>
             </div>
             <Separator />
-            <Button type="submit" className="w-full">
+            <Button disabled={isLoading} type='submit' className='w-full'>
               Executar
-              <Wand2 className="w-4 h-4 ml-2" />
+              <Wand2 className='w-4 h-4 ml-2' />
             </Button>
           </form>
         </aside>
